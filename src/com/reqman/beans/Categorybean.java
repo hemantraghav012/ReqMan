1be8e91362002799f446e93e1b9214194a7d96bb
Naveen Namburu <naveen.namburu@concentrix.com>
2017-09-09 23:41:20 +0530

    category common search completed

--- src/com/reqman/beans/Categorybean.java
+++ src/com/reqman/beans/Categorybean.java
@@ -1,5 +1,7 @@
 package com.reqman.beans;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,10 +9,24 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Row;
+
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
 import com.reqman.dao.CategoryMasterInterface;
 import com.reqman.daoimpl.CategoryMasterImpl;
 import com.reqman.util.SessionUtils;
@@ -18,15 +34,17 @@ import com.reqman.util.UserSession;
 import com.reqman.vo.CategoryVo;
 
 @ManagedBean(name="categorybean",eager = true)
-@RequestScoped
+@ViewScoped
 public class Categorybean implements Serializable
 {
 	private static final long serialVersionUID = 3076255353187837257L;
 	
 	private List<CategoryVo> categoryList = new ArrayList<CategoryVo>();	
+	private List<CategoryVo> filteredCategoryList = new ArrayList<CategoryVo>();
 	private String categoryName;	
 	private Boolean status;	
 	private String categoryId;
+	private CategoryVo selectedCategory;
 	
 	
 	private CategoryMasterInterface categoryMasterInterface = new CategoryMasterImpl();	
@@ -41,6 +59,7 @@ public class Categorybean implements Serializable
 			String userName = (String)session.getAttribute("username");
 			System.out.println("--usersession--userName-->"+userName);
 			categoryList = categoryMasterInterface.getCategoryDetails(userName);
+			setFilteredCategoryList(categoryList);
 		}
 		catch(Exception e)
 		{
@@ -213,6 +232,36 @@ public class Categorybean implements Serializable
 		}
 		return "category";
 	}
+	
+	public void postProcessXLS(Object document) {
+        HSSFWorkbook wb = (HSSFWorkbook) document;
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow header = sheet.getRow(0);
+         
+        HSSFCellStyle cellStyle = wb.createCellStyle();  
+        cellStyle.setFillForegroundColor(HSSFColor.GREEN.index);
+        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+         
+        for(int i=0; i < header.getPhysicalNumberOfCells();i++) {
+            HSSFCell cell = header.getCell(i);
+             
+            cell.setCellStyle(cellStyle);
+        }
+        
+        
+     }
+     
+    public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+        Document pdf = (Document) document;
+        pdf.open();
+        pdf.setPageSize(PageSize.A4);
+ 
+        //ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        //String logo = externalContext.getRealPath("") + File.separator + "resources" + File.separator + "demo" + File.separator + "images" + File.separator + "prime_logo.png";
+         
+        pdf.addTitle("Collabor8");
+    }
+	
 
 	public void addMessage(String summary) 
 	{
@@ -258,6 +307,26 @@ public class Categorybean implements Serializable
 	public void setCategoryId(String categoryId) 
 	{
 		this.categoryId = categoryId;
+	}
+
+
+	public List<CategoryVo> getFilteredCategoryList() {
+		return filteredCategoryList;
+	}
+
+
+	public void setFilteredCategoryList(List<CategoryVo> filteredCategoryList) {
+		this.filteredCategoryList = filteredCategoryList;
+	}
+
+
+	public CategoryVo getSelectedCategory() {
+		return selectedCategory;
+	}
+
+
+	public void setSelectedCategory(CategoryVo selectedCategory) {
+		this.selectedCategory = selectedCategory;
 	}
 
 }
