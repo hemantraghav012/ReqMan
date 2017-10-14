cf529fe31c14cd548d26b70ef157fe537ca66016
hemant raghav <hemantraghav012@gmail.com>
2017-10-14 18:25:34 +0530

    UserupdateVo

---
+++ src/com/reqman/vo/UserupdateVo.java
@@ -0,0 +1,48 @@
+package com.reqman.vo;
+
+public class UserupdateVo {
+
+	   private String emailid;
+	    private String password;
+		private String firstname;
+		private String lastname;
+		private String msg;
+	 
+		private String shortname;
+		public String getEmailid() {
+			return emailid;
+		}
+		public void setEmailid(String emailid) {
+			this.emailid = emailid;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getFirstname() {
+			return firstname;
+		}
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
+		public String getLastname() {
+			return lastname;
+		}
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+		
+		public String getShortname() {
+			return shortname;
+		}
+		public void setShortname(String shortname) {
+			this.shortname = shortname;
+		}
+		
+		
+		
+	
+	
+}
