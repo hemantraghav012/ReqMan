d015a877f8fde98607694f92d728a9c3e1b35213
hemant raghav <hemantraghav012@gmail.com>
2017-10-27 10:55:17 +0530

    userDetailInterface.java daoclass

--- src/com/reqman/dao/UserDetailsInterface.java
+++ src/com/reqman/dao/UserDetailsInterface.java
@@ -11,9 +11,14 @@ public interface UserDetailsInterface
 	
 	public int saveUser(String emailid, String password, String firstname, String lastname, String shortname) throws Exception;
 
-	public int updateUsers(String emailid,String firstname, String lastname, String shortname) throws Exception;
+	public int updateUsers(String emailid,String firstname, String lastname, String shortname, String password) throws Exception;
 
 	public UserupdateVo getUseremailid(String userName)throws Exception;
 
+
+
+	
+
+	
 	
 }
