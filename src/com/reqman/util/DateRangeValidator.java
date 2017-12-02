c0405c4aa6fa70faf69154331bde0afe81b74e55
hemant raghav <hemantraghav012@gmail.com>
2017-12-02 17:46:55 +0530

    util class

---
+++ src/com/reqman/util/DateRangeValidator.java
@@ -0,0 +1,36 @@
+package com.reqman.util;
+
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.primefaces.context.RequestContext;
+
+@FacesValidator(value = "dateRangeValidator")
+public class DateRangeValidator implements Validator {
+  @Override
+  public void validate(FacesContext context,
+                       UIComponent uiComponent, Object o) throws ValidatorException {
+      UIInput startDateInput = (UIInput) uiComponent.getAttributes().get("startDateAttr");
+      Date startDate = (Date) startDateInput.getValue();
+      Date endDate = (Date) o;
+
+      if (endDate.before(startDate)) {
+          FacesMessage msg =
+                  new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                          "Invalid start/end dates.",
+                          "Start date cannot be after end date.");
+          throw new ValidatorException(msg);
+      } else {
+          RequestContext c = RequestContext.getCurrentInstance();
+          c.execute("PF('dateRangeDlg').hide();");
+          c.execute("location.reload();");
+      }
+  }
+}
--- src/com/reqman/util/Dateconverter.java
+++ src/com/reqman/util/Dateconverter.java
@@ -1,25 +1,12 @@
 package com.reqman.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Dateconverter {
 
-	 public static String convertDateToStringDDMMDDYYYY(Date date) {
-		// DateFormat dateTimeInstance = SimpleDateFormat.getDateTimeInstance();
-		 
-	        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-	        String newdate = null;
-	        try {
-	            newdate = df.format(date);
-	            //
-
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	        return newdate;
-
-	    }
+	 
 	
 	 public static String convertTimeToStringhhmmss(Date date) {
 			// DateFormat dateTimeInstance = SimpleDateFormat.getDateTimeInstance();
@@ -36,4 +23,32 @@ public class Dateconverter {
 		        return newtime;
 
 		    }
+	 
+	 
+	 public static String convertDateToStringDDMMDDYYYY(Date date) {
+					 
+		        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		        String newdate = null;
+		        try {
+		            newdate = df.format(date);
+		          
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+		        return newdate;
+
+		    }
+	 public static Date getPreToPreMonthDate(Date date){
+		    SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
+		    Calendar cal = Calendar.getInstance();  
+		    cal.setTime(date);  
+		     
+		    cal.add(Calendar.MONTH, -1);
+
+		    Date preMonthDate = cal.getTime();
+			return preMonthDate;  
+		   
+		}
+	 
 }
