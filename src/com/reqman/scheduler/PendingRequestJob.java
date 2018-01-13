16b66601c342ea3c86d91ffb8db8aa8b91463e2a
hemant raghav <hemantraghav012@gmail.com>
2018-01-13 12:30:36 +0530

    scheduler package

--- src/com/reqman/scheduler/PendingRequestJob.java
+++ src/com/reqman/scheduler/PendingRequestJob.java
@@ -1,16 +1,41 @@
 package com.reqman.scheduler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
+import com.reqman.daoimpl.RequesttypeMasterImpl;
+import com.reqman.util.SessionUtils;
+import com.reqman.util.requestemail;
 
 public class PendingRequestJob implements Job 
 {
 	public void execute(JobExecutionContext context)
 		throws JobExecutionException 
 	{
-
+//	ArrayList<String> alluseremailid=new ArrayList<String>();
+	
+	requestemail rm= new requestemail();	
+		
+		try {
+			RequesttypeMasterImpl reinf = new RequesttypeMasterImpl();
+			List<String> emailList=reinf.AllUser();
+	       System.out.println("requestemail--"+emailList );
+	       for(String userName: emailList){
+			rm.friendemail(userName,userName);
+	       }
+			System.out.println("PendingRequestJob12");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("PendingRequestJob ");
+		
 
 	}
 }
