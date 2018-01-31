2f9fca5f40ad5d7d194b9abab54215a4fa17ca39
hemant raghav <hemantraghav012@gmail.com>
2018-01-31 17:21:54 +0530

    dao package

--- src/com/reqman/dao/AccountInterface.java
+++ src/com/reqman/dao/AccountInterface.java
@@ -2,10 +2,18 @@ package com.reqman.dao;
 
 import java.util.List;
 
+import org.primefaces.model.UploadedFile;
+
 import com.reqman.vo.AccountVo;
 
 public interface AccountInterface {
 
 public	List<AccountVo> getaccountDetails(String userName) throws Exception;
+
+public int updatelogo( String accountname, boolean status, Integer id, String userName, UploadedFile logo, String imagename) throws Exception;
+
+public AccountVo getAccountById(Integer id, String userName)throws Exception;
+
+public byte[] getImageDetails(String userName) throws Exception;
 
 }
